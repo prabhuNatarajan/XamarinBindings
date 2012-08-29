e521372a94d07aa02467ded1d6ea7aac7696905c
rene <>
2012-08-29 21:29:14 +0200

    ...

--- PSPDFKit.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using MonoTouch.ObjCRuntime;
-
-[assembly: LinkWith ("PSPDFKit.a", LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, IsCxx = true, Frameworks = "MediaPlayer CoreText QuartzCore MessageUI ImageIO CoreMedia CoreGraphics AVFoundation", LinkerFlags = "-lz -ObjC")]
