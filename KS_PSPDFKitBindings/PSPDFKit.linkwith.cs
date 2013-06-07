4616db7d532006dcd085ce3f771a77bb696afeeb
Peter Steinberger <me@petersteinberger.com>
2013-06-07 10:37:41 +0200

    Adding missing framework on linking. Fixes #19.

--- KS_PSPDFKitBindings/PSPDFKit.linkwith.cs
+++ KS_PSPDFKitBindings/PSPDFKit.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("PSPDFKit.a", LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, IsCxx = true, Frameworks = "Security AssetsLibrary MediaPlayer CoreText QuartzCore MessageUI ImageIO CoreMedia CoreGraphics AVFoundation", LinkerFlags = "-lz -ObjC -fobjc-arc")]
+[assembly: LinkWith ("PSPDFKit.a", LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, IsCxx = true, Frameworks = "Security AssetsLibrary MediaPlayer CoreText QuartzCore MessageUI ImageIO CoreMedia CoreGraphics AVFoundation AudioToolbox QuickLook", LinkerFlags = "-lz -ObjC -fobjc-arc")]
